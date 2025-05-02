83b8cc14b9086ab4b3c481f68f8dc7e2fefc1dd2
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-05-02 19:16:11 +0000

    all done

--- solution.rb
+++ solution.rb
@@ -1,4 +1,47 @@
 
 def pair_sum(array, target)
   # TODO
+  result = []
+
+  # nested loops
+
+  # for i in 0...array.length-1 do
+  #   for k in i+1...array.length do
+  #     if array[i] + array[k] == target
+  #       result.push([array[i], array[k]])
+  #     end
+  #   end
+  # end
+
+  #complement value option 1
+
+  # track = array.to_set()
+
+  # array.each do |num|
+  #   complement = target - num
+  #   track.delete(num)
+
+  #   if (track.include?(complement))
+  #     result.push([num, complement])
+  #     track.delete(complement)
+  #   end
+
+  # end
+
+  #complement value option 2
+  seen = Set.new
+  array.each do |num|
+    complement = target - num
+    if seen.include?(complement)
+      result.push([num,complement])
+    end
+    seen.add(num)
+  end
+
+  return result
 end
+
+# pp pair_sum([1, 2, 3, 4, 5], 5)
+pp pair_sum([0, -1, 2, -3, 1], -2)
+# pp pair_sum([11, -4, 7, 8, -10], 3)
+
--- tests/test_solution.rb
+++ tests/test_solution.rb
@@ -5,22 +5,23 @@ class TestSolution < Minitest::Test
   def test_pairs_sum_to_5
     result = pair_sum([1, 2, 3, 4, 5], 5)
     expected = [[1, 4], [2, 3]]
-    assert_equal(normalize(result), normalize(expected))
+    assert_equal(normalize(expected), normalize(result))
   end
 
   def test_sum_to_negative_2
     result = pair_sum([0, -1, 2, -3, 1], -2)
     expected = [[-3, 1]]
-    assert_equal(normalize(result), normalize(expected))
+    assert_equal(normalize(expected), normalize(result))
   end
 
   def test_pairs_sum_to_3
     result = pair_sum([11, -4, 7, 8, -10], 3)
     expected = [[-4, 7]]
-    assert_equal(normalize(result), normalize(expected))
+    assert_equal(normalize(expected), normalize(result))
   end
 
   def test_pairs_sum_to_o_n_time_complexity
+    # skip
     large_array = Array.new(100000) { rand(-10000..10000) }
     target_sum = rand(-20000..20000)
     start_time = Time.now
